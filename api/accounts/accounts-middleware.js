28b74c0e8a962c796742a6f1ae33e75eb1af04a8
IrvinA <arevalosirvin@gmail.com>
2021-12-06 09:37:46 -0800

    middlewares complete

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -42,6 +42,19 @@ exports.checkAccountNameUnique = async (req, res, next) => {
   }
 };
 
-exports.checkAccountId = (req, res, next) => {
-  // DO YOUR MAGIC
+exports.checkAccountId = async (req, res, next) => {
+  try {
+    const account = await Accounts.getById(req.params.id);
+    if (!account) {
+      next({
+        status: 404,
+        message: 'account not found',
+      });
+    } else {
+      req.account = account;
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
 };
